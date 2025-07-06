ae8dbb2931fcf73db132d4e13b383812da0f93d9
John Mithamo <johnthamz44@gmail.com>
2025-07-06 16:03:36 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,14 +1,26 @@
--- Create index on users.email to speed up login or search by email
+-- Create index on users.email
 CREATE INDEX idx_users_email ON users(email);
 
--- Create index on bookings.user_id to speed up joins
+-- Create index on bookings.user_id
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 
--- Create index on bookings.property_id to speed up joins with properties
+-- Create index on bookings.property_id
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 
--- Create index on properties.city for quick search by location
+-- Create index on properties.city
 CREATE INDEX idx_properties_city ON properties(city);
 
--- Create index on properties.price for sorting or filtering by price
+-- Create index on properties.price
 CREATE INDEX idx_properties_price ON properties(price);
+
+-- Use EXPLAIN ANALYZE to demonstrate performance
+EXPLAIN ANALYZE SELECT * FROM users WHERE email = 'test@example.com';
+
+EXPLAIN ANALYZE SELECT * FROM bookings WHERE user_id = 5;
+
+EXPLAIN ANALYZE
+SELECT p.*
+FROM properties p
+JOIN bookings b ON p.id = b.property_id
+WHERE p.city = 'Nairobi';
+
